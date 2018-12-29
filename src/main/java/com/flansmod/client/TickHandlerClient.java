62e4f70b5b3aeb8cfcd57a14965dec9aeb5afd69
TheWillyJ <41086203+TheWillyJ@users.noreply.github.com>
2018-12-29 17:23:44 -0400

    Fixed an oopsie

--- src/main/java/com/flansmod/client/TickHandlerClient.java
+++ src/main/java/com/flansmod/client/TickHandlerClient.java
@@ -96,7 +96,7 @@ public class TickHandlerClient
 	{
 		Minecraft mc = Minecraft.getMinecraft();
 		//If main config is set to false, blanket disable crosshairs (client synced)
-		if(!FlansMod.crosshairEnable)
+		if(!FlansMod.crosshairEnable && event.type == ElementType.CROSSHAIRS && mc.thePlayer != null && mc.thePlayer.getHeldItem() != null && mc.thePlayer.getHeldItem().getItem() instanceof ItemGun)
 		{
 			event.setCanceled(true);
 			return;
