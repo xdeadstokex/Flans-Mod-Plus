35c93f6427a28f839bd0cf99a13c3054d5038d61
GolddolphinSKB-Fexcraftian <22482566+GolddolphinSKB-Fexcraftian@users.noreply.github.com>
2021-01-10 21:23:09 +0000

    Version 1.44

--- src/main/java/com/flansmod/common/FlansMod.java
+++ src/main/java/com/flansmod/common/FlansMod.java
@@ -78,7 +78,7 @@ public class FlansMod {
     public static boolean DEBUG = false;
     public static Configuration configFile;
     public static final String MODID = "flansmod";
-    public static final String VERSION = "1.43";
+    public static final String VERSION = "1.44";
     @Instance(MODID)
     public static FlansMod INSTANCE;
     public static boolean printDebugLog = true;
