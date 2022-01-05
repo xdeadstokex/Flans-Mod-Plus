9b8952bc7c274b020f5d71902c165b55a8cceea8
GolddolphinSKB-Fexcraftian <22482566+GolddolphinSKB-Fexcraftian@users.noreply.github.com>
2022-01-05 23:29:58 +0000

    Fix crash when opening repair GUI when vehicle is dying

--- src/main/java/com/flansmod/client/gui/GuiDriveableRepair.java
+++ src/main/java/com/flansmod/client/gui/GuiDriveableRepair.java
@@ -50,14 +50,14 @@ public class GuiDriveableRepair extends GuiScreen {
         driver = player;
         if (player.ridingEntity == null || !((EntitySeat) player.ridingEntity).foundDriveable) {
             Minecraft.getMinecraft().thePlayer.closeScreen();
-        }
-
-        driving = ((EntitySeat) player.ridingEntity).driveable;
-        for (DriveablePart part : driving.getDriveableData().parts.values()) {
-            //Check to see if the part is actually damageable
-            if (part.maxHealth > 0) {
-                //Add it to the list of parts to draw
-                partsToDraw.add(part);
+        } else {
+            driving = ((EntitySeat) player.ridingEntity).driveable;
+            for (DriveablePart part : driving.getDriveableData().parts.values()) {
+                //Check to see if the part is actually damageable
+                if (part.maxHealth > 0) {
+                    //Add it to the list of parts to draw
+                    partsToDraw.add(part);
+                }
             }
         }
     }
