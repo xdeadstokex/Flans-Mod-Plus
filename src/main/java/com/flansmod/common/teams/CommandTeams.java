d54ac4501be378a49a170082ffd0757bbca09ce0
SecretAgent12 <51740316+SecretAgent12@users.noreply.github.com>
2022-02-07 20:28:52 +0200

    big minigames update
    added new commands
    /teams leaderboard
    /teams stats <nickname>

--- src/main/java/com/flansmod/common/teams/CommandTeams.java
+++ src/main/java/com/flansmod/common/teams/CommandTeams.java
@@ -267,15 +267,34 @@ public class CommandTeams extends CommandBase {
             sender.addChatMessage(new ChatComponentText("Autobalance is now " + (TeamsManager.autoBalance ? "enabled" : "disabled")));
             return;
         }
-        if (split[0].equals("useRotation")) {
+
+        if (split[0].equalsIgnoreCase("stats")) {
             if (split.length != 2) {
-                sender.addChatMessage(new ChatComponentText("Incorrect Usage : Should be /teams " + split[0] + " <true/false>"));
+                sender.addChatMessage(new ChatComponentText("Incorrect Usage : Should be /teams " + split[0] + " <nickname>"));
                 return;
             }
-            TeamsManager.voting = !Boolean.parseBoolean(split[1]);
-            sender.addChatMessage(new ChatComponentText("Voting is now " + (TeamsManager.voting ? "enabled" : "disabled")));
-            return;
+            PlayerInfo info = PlayerInfo.getPlayerInfoFromFile(split[1]);
+            if(info==null){
+                sender.addChatMessage(new ChatComponentText("Player not found/Error reading player info"));
+                return;
+            }
+            sender.addChatMessage(new ChatComponentText("\u00a74\u00a7lKills: "+"\u00a7a\u00a7l"+info.kills));
+            sender.addChatMessage(new ChatComponentText("\u00a74\u00a7lDeaths: "+"\u00a7a\u00a7l"+info.deaths));
+            sender.addChatMessage(new ChatComponentText("\u00a74\u00a7lK/D Ratio: "+"\u00a7a\u00a7l"+(double)info.kills/info.deaths));
+            sender.addChatMessage(new ChatComponentText("\u00a74\u00a7lRank: "+"\u00a7a\u00a7l"+info.rank));
+            sender.addChatMessage(new ChatComponentText("\u00a74\u00a7lAVG: "+"\u00a7a\u00a7l"+info.avg));
+            sender.addChatMessage(new ChatComponentText("\u00a74\u00a7lMax Kill Distance: "+"\u00a7a\u00a7l"+info.longestKill));
+            sender.addChatMessage(new ChatComponentText("\u00a74\u00a7lPlayed Rounds: "+"\u00a7a\u00a7l"+info.playedRounds));
+            sender.addChatMessage(new ChatComponentText("\u00a74\u00a7lPlaytime: "+"\u00a7a\u00a7l"+info.playTime));
+            sender.addChatMessage(new ChatComponentText("\u00a74\u00a7lMVP Count: "+"\u00a7a\u00a7l"+info.MVPCount));
+            sender.addChatMessage(new ChatComponentText("\u00a74\u00a7lFlags Captured: "+"\u00a7a\u00a7l"+info.capturedFlags));
+            sender.addChatMessage(new ChatComponentText("\u00a74\u00a7lFlags Saved: "+"\u00a7a\u00a7l"+info.savedFlags));
+            sender.addChatMessage(new ChatComponentText("\u00a74\u00a7lVehicles Destroyed: "+"\u00a7a\u00a7l"+info.kills));
         }
+        if(split[0].equalsIgnoreCase("leaderboard")){
+            PlayerInfo.printLeaderboardExp(sender);
+        }
+
         if (split[0].equals("voting")) {
             if (split.length != 2) {
                 sender.addChatMessage(new ChatComponentText("Incorrect Usage : Should be /teams " + split[0] + " <true/false>"));
@@ -438,7 +457,7 @@ public class CommandTeams extends CommandBase {
                 return;
             }
             TeamsManager.survivalCanBreakVehicles = Boolean.parseBoolean(split[1]);
-            sender.addChatMessage(new ChatComponentText("Vehicles, Planes, Mechas, AAGuns can " + (TeamsManager.survivalCanBreakVehicles? "now" : "no longer") + " be broken by players in survival"));
+            sender.addChatMessage(new ChatComponentText("Vehicles, Planes, Mechas, AAGuns can " + (TeamsManager.survivalCanBreakVehicles ? "now" : "no longer") + " be broken by players in survival"));
             return;
         }
         if (split[0].equals("armourDrops") || split[0].equals("armorDrops")) {
@@ -691,7 +710,9 @@ public class CommandTeams extends CommandBase {
                     "ping",
                     "bltss",
                     "showbltss",
-                    "vehiclesCanZoom");
+                    "vehiclesCanZoom",
+                    "leaderboard",
+                    "stats");
         }
 
         return null;
@@ -759,6 +780,10 @@ public class CommandTeams extends CommandBase {
                 sender.addChatMessage(new ChatComponentText("/teams bltss <0 ... 100> <0 ... 1000>"));
                 sender.addChatMessage(new ChatComponentText("/teams showbltss"));
                 sender.addChatMessage(new ChatComponentText("/teams vehiclesCanZoom <true / false>"));
+                sender.addChatMessage(new ChatComponentText("/teams leaderboard"));
+                sender.addChatMessage(new ChatComponentText("/teams stats <nickname>"));
+
+
                 break;
             }
         }
