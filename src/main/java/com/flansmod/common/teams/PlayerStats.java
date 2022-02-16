d1312e4764db8234ab9963f14969f2b6086df4ca
SecretAgent12 <51740316+SecretAgent12@users.noreply.github.com>
2022-02-16 12:15:46 +0200

    globla player stats fix
    globla player stats reset after server restart fix

--- src/main/java/com/flansmod/common/teams/PlayerStats.java
+++ src/main/java/com/flansmod/common/teams/PlayerStats.java
@@ -1,222 +1,212 @@
-package com.flansmod.common.teams;
+package com.flansmod.common;
 
-import com.flansmod.common.FlansMod;
-import net.minecraft.command.ICommandSender;
+import com.flansmod.common.driveables.EntityDriveable;
+import com.flansmod.common.driveables.EntitySeat;
+import com.flansmod.common.guns.EntityBullet;
+import com.flansmod.common.guns.EntityDamageSourceFlans;
+import com.flansmod.common.guns.EntityGrenade;
+import com.flansmod.common.guns.ShootableType;
+import com.flansmod.common.network.PacketRequestDebug;
+import com.flansmod.common.teams.PlayerStats;
+import com.flansmod.common.teams.TeamsManager;
+import cpw.mods.fml.common.FMLCommonHandler;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.gameevent.PlayerEvent;
+import cpw.mods.fml.common.gameevent.PlayerEvent.PlayerLoggedInEvent;
+import cpw.mods.fml.common.gameevent.PlayerEvent.PlayerLoggedOutEvent;
+import cpw.mods.fml.common.gameevent.PlayerEvent.PlayerRespawnEvent;
+import cpw.mods.fml.relauncher.Side;
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraft.nbt.CompressedStreamTools;
-import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.util.ChatComponentText;
-import net.minecraft.world.World;
+import net.minecraft.world.WorldServer;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.living.LivingAttackEvent;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
 
-import java.io.*;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
-public class PlayerStats {
+@SuppressWarnings("unused")
+public class PlayerHandler {
+    private static final Random rand = new Random();
+    private boolean statsSynced=false;
+    public static Map<String, PlayerData> serverSideData = new HashMap<String, PlayerData>();
+    public static Map<String, PlayerData> clientSideData = new HashMap<String, PlayerData>();
+    public static Map<String, PlayerStats> serverSidePlayerStats = new HashMap<String, PlayerStats>();
+    public static Map<String, PlayerStats> clientSidePlayerStats = new HashMap<String, PlayerStats>();
+    public static ArrayList<String> clientsToRemoveAfterThisRound = new ArrayList<String>();
 
-    private World world;
-    private EntityPlayerMP playerMP;
-    public String nickname = "NaN";
-    public int kills = 0; //done
-    public int deaths = 0; //done
-    public int exp = 0; //done
-    public int totalExp = 0; //done
-    public int rank = 1; //done
-    public double avg = 0; //done
-    public double longestKill = 0; //done
-    public int playedRounds = 0; //done
-    public double playTime = 0;
-    public int MVPCount = 0; //done
-    public int capturedFlags = 0; //done
-    public int savedFlags = 0; //done
-    public int vehiclesDestroyed = 0; //done 75%
-
-    public PlayerStats(World world, EntityPlayerMP player) {
-        playerMP = player;
-        this.world = world;
-        nickname = player.getDisplayName();
-        savePlayerStats();
+    public PlayerHandler() {
+        MinecraftForge.EVENT_BUS.register(this);
+        FMLCommonHandler.instance().bus().register(this);
     }
 
-    private PlayerStats() {
-
-    }
-
-    public void savePlayerStats() {
-        File dir = new File(world.getSaveHandler().getWorldDirectory() + "\\FlansMod players statistics\\");
-        dir.mkdirs();
-        dir.setReadable(true);
-        dir.setWritable(true);
-        File file = new File(dir, playerMP.getDisplayName() + " " + playerMP.getUniqueID().toString() + ".dat");
-        dir.mkdirs();
-        dir.setReadable(true);
-        dir.setWritable(true);
-        checkFileExists(file);
-        try {
-            NBTTagCompound tags = new NBTTagCompound();
-
-            tags.setString("Nickname", playerMP.getDisplayName());
-            tags.setInteger("Kills", kills);
-            tags.setInteger("Deaths", deaths);
-            tags.setInteger("Exp", exp);
-            tags.setInteger("Total Exp", totalExp);
-            tags.setInteger("Rank", rank);
-            tags.setDouble("AVG", avg);
-            tags.setDouble("Longest Kill", longestKill);
-            tags.setInteger("Rounds Played", playedRounds);
-            tags.setDouble("Play Time", playTime);
-            tags.setInteger("MVP Count", MVPCount);
-            tags.setInteger("Flags Captured", capturedFlags);
-            tags.setInteger("Flags Saved", savedFlags);
-            tags.setInteger("Vehicles Destroyed", vehiclesDestroyed);
-
-
-            CompressedStreamTools.write(tags, new DataOutputStream(new FileOutputStream(file)));
-        } catch (Exception e) {
-            FlansMod.log("Failed to save to teams.dat");
-            e.printStackTrace();
+    @SubscribeEvent
+    public void onEntityHurt(LivingAttackEvent event) {
+        EntityLivingBase entity = event.entityLiving;
+        if (entity.ridingEntity instanceof EntityDriveable || entity.ridingEntity instanceof EntitySeat) {
+            event.setCanceled(true);
         }
     }
 
+    @SubscribeEvent
+    public void onLivingHurtEvent(LivingHurtEvent event) {
+        float damage = event.ammount;
+        if (damage > 0 && event.source instanceof EntityDamageSourceFlans) {
+            EntityDamageSourceFlans source = (EntityDamageSourceFlans) event.source;
 
-    private static boolean checkFileExists(File file) {
-        if (!file.exists()) {
-            try {
-                file.createNewFile();
-                FlansMod.log("Created new file");
-            } catch (Exception e) {
-                FlansMod.log("Failed to create file");
-                FlansMod.log(file.getAbsolutePath());
-                e.printStackTrace();
+            ShootableType shootableType = null;
+            Entity damageSouceEntity = source.getDamageSourceEntity();
+            if (damageSouceEntity instanceof EntityBullet) {
+                shootableType = ((EntityBullet) damageSouceEntity).type;
             }
-            return false;
-        }
-        return true;
-    }
+            if (damageSouceEntity instanceof EntityGrenade) {
+                shootableType = ((EntityGrenade) damageSouceEntity).type;
+            }
+            if (shootableType != null && shootableType.ignoreArmorProbability > 0 && rand.nextFloat() < shootableType.ignoreArmorProbability) {
+                EntityLivingBase entity = event.entityLiving;
+                float f1 = damage;
+                damage = Math.max(damage - entity.getAbsorptionAmount(), 0.0F);
+                entity.setAbsorptionAmount(entity.getAbsorptionAmount() - (f1 - damage));
 
-    public void addExp(float a) {
-        exp += a;
-        totalExp += a;
-        if (exp >= 1000) {
-            rank++;
-            exp = exp - 1000;
-        }
-    }
+                damage *= shootableType.ignoreArmorDamageFactor;
 
-    public void updateLongestKill(float distance) {
-        addExp(distance / 10);
-        if (distance > longestKill) {
-            longestKill = distance;
-        }
-    }
+                if (damage != 0.0F) {
+                    float health = entity.getHealth();
+                    entity.setHealth(health - damage);
+                    entity.func_110142_aN().func_94547_a(source, health, damage);
+                    entity.setAbsorptionAmount(entity.getAbsorptionAmount() - damage);
 
-    public void updateAVG() {
-        avg = (double) kills / playedRounds;
-    }
-
-
-    public static PlayerStats getPlayerStatsFromFile(String name) {
-        PlayerStats toSend = new PlayerStats();
-        File dir = new File(MinecraftServer.getServer().getEntityWorld().getSaveHandler().getWorldDirectory() + "\\FlansMod players statistics\\");
-        for (File file : dir.listFiles()) {
-            if (file.getName().startsWith(name)) {
-                checkFileExists(file);
-                try {
-                    NBTTagCompound tags = CompressedStreamTools.read(new DataInputStream(new FileInputStream(file)));
-                    toSend.nickname = tags.getString("Nickname");
-                    toSend.kills = tags.getInteger("Kills");
-                    toSend.deaths = tags.getInteger("Deaths");
-                    toSend.exp = tags.getInteger("Exp");
-                    toSend.totalExp = tags.getInteger("Total Exp");
-                    toSend.rank = tags.getInteger("Rank");
-                    toSend.avg = tags.getDouble("AVG");
-                    toSend.longestKill = tags.getDouble("Longest Kill");
-                    toSend.playedRounds = tags.getInteger("Rounds Played");
-                    toSend.playTime = tags.getDouble("Play Time");
-                    toSend.MVPCount = tags.getInteger("MVP Count");
-                    toSend.capturedFlags = tags.getInteger("Flags Captured");
-                    toSend.savedFlags = tags.getInteger("Flags Saved");
-                    toSend.vehiclesDestroyed = tags.getInteger("Vehicles Destroyed");
-                    return toSend;
-                } catch (Exception e) {
-                    FlansMod.log("Failed to save to teams.dat");
-                    e.printStackTrace();
-                    return null;
+//					FlansMod.log("Ignore armor damage = " + damage);
                 }
+
+                event.setCanceled(true);
             }
         }
-        return null;
     }
 
-    public static void printLeaderboardExp(ICommandSender sender) {
-        List<String> nameList = new ArrayList<>();
-        File dir = new File(MinecraftServer.getServer().getEntityWorld().getSaveHandler().getWorldDirectory() + "\\FlansMod players statistics\\");
-        for (File file : dir.listFiles()) {
-            checkFileExists(file);
-            try {
-                NBTTagCompound tags = CompressedStreamTools.read(new DataInputStream(new FileInputStream(file)));
-                String nickname = tags.getString("Nickname");
-                nameList.add(nickname);
-            } catch (Exception e) {
-                FlansMod.log("Failed to print leaderboard");
-                e.printStackTrace();
-            }
+    @SubscribeEvent
+    public void onEntityKilled(LivingDeathEvent event) {
+        EntityLivingBase entity = event.entityLiving;
+        if (entity instanceof EntityPlayer) {
+            getPlayerData((EntityPlayer) entity).playerKilled();
         }
-        Collections.sort(nameList, new ComparatorExp());
-        int counter1 = 0;
-        for (String name : nameList) {
-            switch (counter1) {
-                case 0:
-                    sender.addChatMessage(new ChatComponentText("\u00a76\u00a7l1. " + name + " - Rank " + getPlayerStatsFromFile(name).rank + "(" + getPlayerStatsFromFile(name).totalExp + " Exp)"));
-                    break;
-                case 1:
-                    sender.addChatMessage(new ChatComponentText("\u00a74\u00a7l2. " + name + " - Rank " + getPlayerStatsFromFile(name).rank + "(" + getPlayerStatsFromFile(name).totalExp + " Exp)"));
-                    break;
-                case 2:
-                    sender.addChatMessage(new ChatComponentText("\u00a7a\u00a7l3. " + name + " - Rank " + getPlayerStatsFromFile(name).rank + "(" + getPlayerStatsFromFile(name).totalExp + " Exp)"));
-                    break;
-                case 3:
-                    sender.addChatMessage(new ChatComponentText("\u00a7l4. " + name + " - Rank " + getPlayerStatsFromFile(name).rank + "(" + getPlayerStatsFromFile(name).totalExp + " Exp)"));
-                    break;
-                case 4:
-                    sender.addChatMessage(new ChatComponentText("\u00a7l5. " + name + " - Rank " + getPlayerStatsFromFile(name).rank + "(" + getPlayerStatsFromFile(name).totalExp + " Exp)"));
-                    break;
-                case 5:
-                    sender.addChatMessage(new ChatComponentText("\u00a7l6. " + name + " - Rank " + getPlayerStatsFromFile(name).rank + "(" + getPlayerStatsFromFile(name).totalExp + " Exp)"));
-                    break;
-                case 6:
-                    sender.addChatMessage(new ChatComponentText("\u00a7l7. " + name + " - Rank " + getPlayerStatsFromFile(name).rank + "(" + getPlayerStatsFromFile(name).totalExp + " Exp)"));
-                    break;
-                case 7:
-                    sender.addChatMessage(new ChatComponentText("\u00a7l8. " + name + " - Rank " + getPlayerStatsFromFile(name).rank + "(" + getPlayerStatsFromFile(name).totalExp + " Exp)"));
-                    break;
-                case 8:
-                    sender.addChatMessage(new ChatComponentText("\u00a7l9. " + name + " - Rank " + getPlayerStatsFromFile(name).rank + "(" + getPlayerStatsFromFile(name).totalExp + " Exp)"));
-                    break;
-                case 9:
-                    sender.addChatMessage(new ChatComponentText("\u00a7l10. " + name + " - Rank " + getPlayerStatsFromFile(name).rank + "(" + getPlayerStatsFromFile(name).totalExp + " Exp)"));
-                    break;
-                case 10:
-                    sender.addChatMessage(new ChatComponentText("\u00a7b\u00a7l"+nameList.indexOf(sender.getCommandSenderName())+1+". " + sender.getCommandSenderName() + " - Rank " + getPlayerStatsFromFile(name).rank + "(" + getPlayerStatsFromFile(name).totalExp + " Exp)"));
-                    break;
-            }
-            counter1++;
-            if (counter1 >= 11) break;
-        }
-        counter1 = 0;
     }
-}
 
-class ComparatorExp implements Comparator<String> {
-    @Override
-    public int compare(String a, String b) {
-        PlayerStats info1 = PlayerStats.getPlayerStatsFromFile(a);
-        PlayerStats info2 = PlayerStats.getPlayerStatsFromFile(b);
-        if (info1 == null || info2 == null)
-            return 0;
-        return info2.totalExp - info1.totalExp;
+    public void serverTick() {
+        for (WorldServer world : MinecraftServer.getServer().worldServers) {
+            for (Object player : world.playerEntities) {
+                getPlayerData((EntityPlayer) player).tick((EntityPlayer) player);
+            }
+        }
+        if(!statsSynced){
+            for(PlayerStats stats : PlayerStats.getAllPlayersStats()){
+                serverSidePlayerStats.put(stats.nickname,stats);
+                statsSynced=true;
+            }
+        }
+    }
+
+    public void clientTick() {
+        if (Minecraft.getMinecraft().theWorld != null) {
+            for (Object player : Minecraft.getMinecraft().theWorld.playerEntities) {
+                getPlayerData((EntityPlayer) player).tick((EntityPlayer) player);
+            }
+        }
+    }
+
+    public static PlayerData getPlayerData(EntityPlayer player) {
+        if (player == null)
+            return null;
+        return getPlayerData(player.getCommandSenderName(), player.worldObj.isRemote ? Side.CLIENT : Side.SERVER);
+    }
+
+    public static PlayerData getPlayerData(String username) {
+        return getPlayerData(username, Side.SERVER);
+    }
+
+    public static PlayerData getPlayerData(EntityPlayer player, Side side) {
+        if (player == null)
+            return null;
+        return getPlayerData(player.getCommandSenderName(), side);
+    }
+
+    public static PlayerData getPlayerData(String username, Side side) {
+        if (side.isClient()) {
+            if (!clientSideData.containsKey(username))
+                clientSideData.put(username, new PlayerData(username));
+        } else {
+            if (!serverSideData.containsKey(username))
+                serverSideData.put(username, new PlayerData(username));
+        }
+        return side.isClient() ? clientSideData.get(username) : serverSideData.get(username);
+    }
+
+    //---
+
+    public static PlayerStats getPlayerStats(EntityPlayerMP player) {
+        if (player == null)
+            return null;
+        return getPlayerStats(player, player.worldObj.isRemote ? Side.CLIENT : Side.SERVER);
+    }
+
+    public static PlayerStats getPlayerStats(EntityPlayerMP player, Side side) {
+        String username = player.getCommandSenderName();
+        if (side.isClient()) {
+            if (!clientSidePlayerStats.containsKey(username))
+                clientSidePlayerStats.put(username, new PlayerStats(player.worldObj, player));
+        } else {
+            if (!serverSidePlayerStats.containsKey(username))
+                serverSidePlayerStats.put(username, new PlayerStats(player.worldObj, player));
+        }
+        return side.isClient() ? clientSidePlayerStats.get(username) : serverSidePlayerStats.get(username);
+    }
+
+    @SubscribeEvent
+    public void onPlayerEvent(PlayerEvent event) {
+        if (event instanceof PlayerLoggedInEvent) {
+            if (event.player instanceof EntityPlayerMP) {
+                FlansMod.packetHandler.sendTo(new PacketRequestDebug(false), (EntityPlayerMP) event.player);
+            }
+
+            EntityPlayer player = event.player;
+            String username = player.getCommandSenderName();
+            if (!serverSideData.containsKey(username))
+                serverSideData.put(username, new PlayerData(username));
+            clientsToRemoveAfterThisRound.remove(username);
+            if (!serverSidePlayerStats.containsKey(username))
+                serverSidePlayerStats.put(username, new PlayerStats(player.worldObj, (EntityPlayerMP)player));
+
+        } else if (event instanceof PlayerLoggedOutEvent) {
+            EntityPlayer player = event.player;
+            String username = player.getCommandSenderName();
+            if (TeamsManager.getInstance().currentRound == null)
+                serverSideData.remove(username);
+            else clientsToRemoveAfterThisRound.add(username);
+        } else if (event instanceof PlayerRespawnEvent) {
+            EntityPlayer player = event.player;
+            String username = player.getCommandSenderName();
+            if (!serverSideData.containsKey(username))
+                serverSideData.put(username, new PlayerData(username));
+            if (!serverSidePlayerStats.containsKey(username))
+                serverSidePlayerStats.put(username, new PlayerStats(player.worldObj, (EntityPlayerMP)player));
+
+        }
+    }
+
+    /**
+     * Called by teams manager to remove lingering player data
+     */
+    public static void roundEnded() {
+        for (String username : clientsToRemoveAfterThisRound) {
+            serverSideData.remove(username);
+        }
     }
 }
