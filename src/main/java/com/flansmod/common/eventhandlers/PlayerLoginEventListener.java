23bbc91adfccd30ad4270a906c8cacdc5b01c370
Unknown025 <11259556+Unknown025@users.noreply.github.com>
2024-06-30 12:58:12 -0700

    Added obfuscated name for food stats registering.

--- src/main/java/com/flansmod/common/eventhandlers/PlayerLoginEventListener.java
+++ src/main/java/com/flansmod/common/eventhandlers/PlayerLoginEventListener.java
@@ -3,11 +3,10 @@ package com.flansmod.common.eventhandlers;
 import com.flansmod.common.CustomFoodStats;
 import com.flansmod.common.FlansMod;
 import com.flansmod.common.network.PacketModConfig;
-
 import cpw.mods.fml.common.FMLCommonHandler;
-import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.PlayerEvent.PlayerLoggedInEvent;
+import cpw.mods.fml.relauncher.ReflectionHelper;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.attributes.IAttributeInstance;
 import net.minecraft.entity.player.EntityPlayer;
@@ -16,27 +15,25 @@ import net.minecraft.entity.player.EntityPlayerMP;
 import java.lang.reflect.Field;
 
 public class PlayerLoginEventListener {
+    public PlayerLoginEventListener() {
+        FMLCommonHandler.instance().bus().register(this);
+    }
 
-	public PlayerLoginEventListener() {
-		FMLCommonHandler.instance().bus().register(this);
-	}
+    @SubscribeEvent
+    public void OnPlayerLogin(PlayerLoggedInEvent event) {
+        //Sync the players config with the servers config
+        FlansMod.getPacketHandler().sendTo(new PacketModConfig(), (EntityPlayerMP) event.player);
 
-	@EventHandler
-	@SubscribeEvent
-	public void OnPlayerLogin(PlayerLoggedInEvent event) {
-		//Sync the players config with the servers config
-		FlansMod.getPacketHandler().sendTo(new PacketModConfig(), (EntityPlayerMP) event.player);
+        //Set the players max health
+        IAttributeInstance attribute = event.player.getAttributeMap().getAttributeInstance(SharedMonsterAttributes.maxHealth);
+        attribute.setBaseValue(FlansMod.maxHealth);
 
-		//Set the players max health
-		IAttributeInstance attribute = event.player.getAttributeMap().getAttributeInstance(SharedMonsterAttributes.maxHealth);
-		attribute.setBaseValue(FlansMod.maxHealth);
-
-		try {
-			Field foodStatsField = EntityPlayer.class.getDeclaredField("foodStats");
-			foodStatsField.setAccessible(true);
-			foodStatsField.set(event.player, new CustomFoodStats());
-		} catch (NoSuchFieldException | IllegalAccessException e) {
-			e.printStackTrace();
-		}
-	}
+        try {
+            Field foodStatsField = ReflectionHelper.findField(EntityPlayer.class, "foodStats", "field_71100_bB");
+            foodStatsField.setAccessible(true);
+            foodStatsField.set(event.player, new CustomFoodStats());
+        } catch (ReflectionHelper.UnableToFindFieldException | IllegalAccessException e) {
+            FlansMod.logException("Couldn't set food stats", e);
+        }
+    }
 }
