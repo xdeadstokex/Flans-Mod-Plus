0ec467fe12d2cac099ae36e3b7f26294dd7af197
xdeadstokex <129391255+xdeadstokex@users.noreply.github.com>
2024-10-19 01:26:53 +0700

    clean some codes BlockFlansWorkbench.java
    The onBlockActivated code clean up seem to be longer but I avoid using switch when the case is small and using int, boolean to avoid calling function multiple times

--- src/main/java/com/flansmod/common/BlockFlansWorkbench.java
+++ src/main/java/com/flansmod/common/BlockFlansWorkbench.java
@@ -43,29 +43,21 @@ public class BlockFlansWorkbench extends Block
     @Override
     public IIcon getIcon(int i, int j)
     {
-        if(i == 1)
-        {
-            if (j <= 2 && j >= 0) {
-                return top[j];
-            } else {
-                return side;
-            }
-            
-        } else
-        {
-            return side;
-        }
+	if (i == 1) return (j >= 0 && j <= 2) ? top[j] : side;
+	return side;
     }
 
     @Override
     public boolean onBlockActivated(World world, int i, int j, int k, EntityPlayer entityplayer, int par6, float par7, float par8, float par9)
     {
-    	switch(world.getBlockMetadata(i, j, k))
-    	{
-    	case 0 : if(world.isRemote) entityplayer.openGui(FlansMod.INSTANCE, 0, world, i, j, k); break;
-    	case 1 : if(!world.isRemote) entityplayer.openGui(FlansMod.INSTANCE, 2, world, i, j, k); break; 
-    	}    	
-		return true;
+	int metadata = world.getBlockMetadata(i, j, k);
+	boolean isRemote = world.isRemote;
+	int guiId = -1;
+
+	if (metadata == 0 && isRemote) guiId = 0;
+	else if (metadata == 1 && !isRemote) guiId = 2;
+	if (guiId != -1) entityplayer.openGui(FlansMod.INSTANCE, guiId, world, i, j, k); 	
+	return true;
     }
     
     @Override
